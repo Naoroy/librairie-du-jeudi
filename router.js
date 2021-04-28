b3e82d428fc0941530b7a5fe6b2be7925fbea101
Naoroy <fegan.pierre@gmail.com>
2021-04-28 17:38:18 +0200

    Add status on root route, add get book by id function

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ const db = require('./firebase_connect')
 const Books = db.ref('books')
 
 router
-  .get('/',           toDo)
+  .get('/',           map)
   .get('/books',      getBooks)
   .post('/book',      createBook)
   .put('/book:id',    updateBook)
@@ -16,45 +16,72 @@ router
  * if query = name, search books by name
  */
 function getBooks(req, res) {
-  const id = req.params.id
-  const name = req.params.name
-  let books = []
+  const id = req.query.id
+  const name = req.query.name
 
+  if (id) {
+    getBooksByID(id)
+      .then((books) => {
+        res.send(books)
+      })
+      .catch((error) => res.status(404).send("Not found"))
+  } else {
+    getAllBooks()
+      .then((books) => {
+        res.send(books)
+      })
+      .catch((error) => res.status(404).send("Not found"))
+  }
   /* Query database.books, data is stored in snapshot */
-  Books.once('value').then(snapshot => {
-    snapshot.forEach(child => {
-      books.push({ id: child.key, ...child.val() })
-    })
+  //Books.once('value').then(snapshot => {
+  //  console.log("book?")
+  //  const books = []
+  //  snapshot.forEach(child => {
+  //    books.push({ id: child.key, ...child.val() })
+  //  })
+  //  res.send(books)
+  //})
+  //.catch(error => res.send(error))
+  //if (false) {
+  //  books = getBooksByID(id)
+  //  console.log(id)
+  //  res.send()
+  //  return
+  //}
+  //else if (name) books = getBooksByName(name)
 
-    res.send({ books })
+}
+
+function getAllBooks() {
+  return new Promise((fulfill, reject) => {
+    Books.once('value').then(snapshot => {
+      console.log("book?")
+      const books = []
+      snapshot.forEach(child => {
+        books.push({ id: child.key, ...child.val() })
+      })
+      fulfill(books)
+    })
+    .catch(error => reject(error))
   })
-  .catch(error => res.send(error))
-  /*
-  if (id) books = getBooksByID(id)
-  else if (name) books = getBooksByName(name)
-  else books = getAllBooks(res)
-  */
 }
 
-function getAllBooks(res) {
+function getBooksByID(bookID) {
+  return new Promise((fulfill, reject) => {
+    Books.once('value').then(snapshot => {
+      const books = []
 
-}
+      snapshot.forEach(child => {
+        const id = child.key
+        const data = child.val()
 
-function getBooksByID(res, bookID) {
-  const books = []
+        if (id.length < 5) return
+        else if (id.search(bookID) == -1) { return }
+        else { books.push({ id: id, ...data }) }
+      })
 
-  Books.once('value').then(snapshot => {
-    snapshot.forEach(child => {
-      const id = child.key
-      const data = child.val()
-
-      if (id.length < 5) return
-      else if (bookID.search(id) == -1) { return }
-
-      books.push({ id: id, ...data })
+      fulfill(books)
     })
-
-  res.send({books})
   })
 }
 
@@ -84,6 +111,22 @@ function updateBook(req, res) {
 
 function deleteBook(req, res) {
 
+}
+
+function map(req, res) {
+  res.send(`
+  <p>
+  <span style="color:red">à faire</span>
+  <span style="color:orange">en cours</span>
+  <span style="color:green">fini</span>
+  </p>
+  <hr>
+  <p style="color:green">GET /books</p>
+  <p style="color:green">GET /books?id=(au moins 5 char de l'id)</p>
+  <p style="color:orange">POST /book</p>
+  <p style="color:red">PUT /book</p>
+  <p style="color:red">DELETE /book?id=(au moins 5 char de l'id)</p>
+  `)
 }
 
 function toDo(req, res) {

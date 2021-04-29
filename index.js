3f19838611340b407eadb921cb3273b3f34067aa
Fégan <fegan.pierre@gmail.com>
2021-04-29 17:00:00 +0200

    Add CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const router = require('./router')
 const PORT = process.env.PORT || 3003
 
+app.use(cors())
 app.use(express.json())
 //app.use(helmet)
 app.use(router)
